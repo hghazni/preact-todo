72b076ae4bef07a6f6281be2e2ef5708bc9ba635
Harry Ghazni <hghazni@gmail.com>
2019-10-28 15:59:06 +0000

    Formats spacing / tidies it up

--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -5,20 +5,28 @@ import style from './style.css';
 const ToDo = () => {
 	const [todos, setTodos] = useState([]);
 	const [text, setText] = useState('');
-	console.log(todos)
+
 	return (<div class={style.todo}>
 		<h1 class="title">ToDo</h1>
+
 		{todos.map((item, i) => {
 			return (<ul class="todoList">
 				<li key={i}>{item}</li>
 			</ul>)
 		})}
-			<input type="text" placeholder="Add a todo item here..." class="todoInput" onChange={(e) => {setText(e.target.value)}}/>
-			<button type="button" class="updateBtn" onClick={() => {
-				const newTodos = todos.concat(text);
-				console.log(newTodos)
-				setTodos(newTodos);
-			}}>Add Task</button>
+
+		<input type="text"
+		placeholder="Add a todo item here..."
+		class="todoInput"
+		onChange={(e) => {setText(e.target.value)}}/>
+
+		<button
+			type="button"
+			class="updateBtn"
+			onClick={() => {
+			const newTodos = todos.concat(text);
+			setTodos(newTodos);
+		}}>Add Task</button>
 	</div>
 	)
 };
