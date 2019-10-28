00371796cc8fe2b928099b23fd88f441d64b947e
Harry Ghazni <hghazni@gmail.com>
2019-10-28 12:35:56 +0000

    initial build

---
+++ src/components/todo/todo.js
@@ -0,0 +1,26 @@
+import { h, Preact } from 'preact';
+import { useState } from 'preact/hooks';
+import style from './style.css';
+
+const ToDo = () => {
+	const [todos, setTodos] = useState([]);
+	const [text, setText] = useState('');
+	console.log(todos)
+	return (<div class={style.todo}>
+		<h1 class="title">ToDo</h1>
+		{todos.map((item, i) => {
+			return (<ul class="todoList">
+				<li key={i}>{item}</li>
+			</ul>)
+		})}
+			<input type="text" placeholder="Add a todo item here..." class="todoInput" onChange={(e) => {setText(e.target.value)}}/>
+			<button type="button" class="updateBtn" onClick={() => {
+				const newTodos = todos.concat(text);
+				console.log(newTodos)
+				setTodos(newTodos);
+			}}>Add Task</button>
+	</div>
+	)
+};
+
+export default ToDo;
